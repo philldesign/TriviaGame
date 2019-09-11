10102bb43060c0336b43d958aa3eb864bed4cb0d
Alfie <53308938+philldesign@users.noreply.github.com>
2019-09-10 23:39:50 -0400

    Countdown & Radios Aded

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,35 @@
 $(document).ready(function () {
 
-    $("#trivia").click(function () {
+    var timeLeft = 30;
+    var elem = document.getElementById('countdown');
+    var timerId = setInterval(countdown, 1000);
 
+
+    function countdown() {
+        if (timeLeft == -1) {
+            clearTimeout(timerId);
+            elem.innerHTML = timeLeft + ' Seconds Remaining'; }
+
+           else if (timeLeft == -1) {
+                clearTimeout(timerId);
+                doSomething();
+                elem.innerHTML = timeLeft + ' Seconds Remaining';
+            }
+        
+        
+        else {
+            elem.innerHTML = timeLeft + ' Seconds Remaining';
+            timeLeft--;
+        }
+
+    }
    
-    console.log(1);
+   
+    console.log($( "input[name=cgi]:radio" ).val("Toy Story")); 
 
-});
+
+
+
 
 
 });
